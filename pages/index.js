a5ef51c4572793060ef69fa920bdf5caaffa30a9
brian-dg <73866002+brian-dg@users.noreply.github.com>
2021-12-07 17:13:54 -0300

    Pasando a next

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return(
+    <h1>E-commerce</h1>
+  )
+}
+
+export default Home;
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">Mi primer e-commerce</div>;
-}
-
-export default App;
